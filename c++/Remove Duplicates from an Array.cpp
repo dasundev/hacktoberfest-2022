b1d7b2165777f0aa02f963e5f3846387e399e2b1
flow6979 <vaibhav84334@gmail.com>
2022-09-28 19:14:45 +0530

    removal of duplicates from an array by vaibhav-dixit-flow6979

---
+++ c++/Remove Duplicates from an Array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"size of array is : ";
+    long long int n,t=INT_MIN;
+    cin>>n;
+    vector<long long int> v;
+    cout<<"insert the elements of the array : ";
+    for(long long int i=0;i<n;i++){
+        long long int temp;
+        cin>>temp;
+        if(temp!=t){
+            v.push_back(temp);
+            t=temp;
+        }
+    }
+    cout<<"your array without any duplicate elements is : ";
+    for(auto k:v){cout<<k<<" ";}
+}
