86bc6776916a2ade85c45159f80bb38cf80ad4c3
flow6979 <vaibhav84334@gmail.com>
2022-09-29 17:32:05 +0530

    new folder created with files placed in it.

---
+++ c++/flow6979/Convert Yen to USD.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Your yen amount : ";
+    long long int n;
+    cin>>n;
+    cout<<"your amount converted into USD : ";
+    cout<< n*0.0069;
+}
+
+//vaibhav-dixit-flow6979
