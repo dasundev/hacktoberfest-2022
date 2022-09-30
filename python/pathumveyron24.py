46d5ccfdeaea7b0197fdfb8171d33f721e4723b7
pathum-bl <pathumg@booleanlabs.biz>
2022-10-01 01:29:11 +0530

    Reverse Array in Python Using Slicing #8

--- python/pathumveyron24.py
+++ python/pathumveyron24.py
@@ -1,18 +1,7 @@
-#Reverse an Array
+#Reverse an Array #8
 
 arr = [1, 2, 3, 4, 5, 6]
 
 reverse_array = arr[::-1]
 
 print("The Reversed Array: ", reverse_array)
-
-
-#Return the Sum of Two Numbers
-
-def sum_of_two_number(first_number, second_number):
-    return first_number + second_number
-
-first_number = int(input("Enter First Number: "))
-second_number = int(input("Enter Second Number: "))
-
-print("Sum Of Two Numbers: " ,sum_of_two_number(first_number, second_number))
