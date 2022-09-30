756c6646740f979aaeffbd8b2ff6fe929a0fb9d7
Vaibhav Dixit <80835305+flow6979@users.noreply.github.com>
2022-09-30 17:54:54 +0530

    added new function to solve " find the largest number in an array "

--- c++/flow6979.cpp
+++ c++/flow6979.cpp
@@ -20,7 +20,24 @@ void RemoveDuplicatesFromAnArray(){
     cout<<"your array without any duplicate elements is : ";
     for(auto k:v){cout<<k<<" ";}
 }
+
+//2.find the largest number in an array
+
+void LargestNumberInArray(){
+    cout<<"size of array is : ";
+    long long int n;
+    cin>>n;
+    vector<int> v(n);
+    cout<<"insert the elements of the array : ";
+    for(long long int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    cout<<"largest number in th array is : "<<v[n-1]<<endl;
+}
+
 int main(){
-    RemoveDuplicatesFromAnArray();
+    //RemoveDuplicatesFromAnArray();
+    LargestNumberInArray();
 }
 //vaibhav-dixit-flow6979
