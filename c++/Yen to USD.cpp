2f8c7b2807fd716dc6926a79180280e3b2c7dd27
Vaibhav Dixit <80835305+flow6979@users.noreply.github.com>
2022-10-01 05:00:01 +0530

    Create Yen to USD.cpp

---
+++ c++/Yen to USD.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//4.Yen to USD
+
+void YenToUSD(){
+    cout<<"Your yen amount : ";
+    long long int n;
+    cin>>n;
+    cout<<"your amount converted into USD : ";
+    long long int USD =n*0.0069;
+    cout<<USD;
+}
+
+int main(){
+    YenToUSD();
+}
+//vaibhav-dixit-flow6979
