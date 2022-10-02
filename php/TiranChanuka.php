e6cbdc01701d93a571ad67e33587d13e2ddff064
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-10-02 23:06:39 +0530

    remove duplicates in an array

--- php/TiranChanuka.php
+++ php/TiranChanuka.php
@@ -1,7 +1,7 @@
 <?php 
 
-function removeDuplicatesinArray($array){
+function removeDuplicateinArray($array){
     return array_unique($array, SORT_REGULAR);
 }
-print_r(removeDuplicatesinArray([1,10,5,4,2,5,7,9,10,11]));
+print_r(removeDuplicateinArray([1,10,5,4,2,5,7,9,10,11]));
 ?>
