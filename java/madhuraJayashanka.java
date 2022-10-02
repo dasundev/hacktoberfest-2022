f1b64a241357e6a85c263b0c2062d32482576c32
Madhura <madhuraweerasooriye@gmail.com>
2022-10-02 13:07:21 +0530

    Add Even or Odd in Java

--- java/madhuraJayashanka.java
+++ java/madhuraJayashanka.java
@@ -1,16 +1,12 @@
 public class madhuraJayashanka {
 
-    public static int sumOfOdd(int[] array){
-        int total=0;
-        for (int element:
-             array) {
-            total += element % 2 == 0 ? 0 : element;
-        }
-        return total;
+    public static String evenOrOdd(int num){
+        return num % 2 == 0 ? "Even" : "Odd";
     }
 
+
     public static void main(String[] args) {
-        int[] array = {2,5,7,3,67,23,78,256};
-        System.out.println(sumOfOdd(array));
+        System.out.println(evenOrOdd(5));
+        System.out.println(evenOrOdd(6));
     }
 }
