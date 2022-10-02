6614b09a2585dd162661e0872b575cce6ae5c77d
Madhura <madhuraweerasooriye@gmail.com>
2022-10-02 13:11:39 +0530

    Add Sum of an Array in Java

--- java/madhuraJayashanka.java
+++ java/madhuraJayashanka.java
@@ -1,12 +1,16 @@
 public class madhuraJayashanka {
 
-    public static String evenOrOdd(int num){
-        return num % 2 == 0 ? "Even" : "Odd";
+    public static int sumArray(int[] array){
+        int sum=0;
+        for (int i = 0; i < array.length; i++) {
+            sum+=array[i];
+        }
+        return sum;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(evenOrOdd(5));
-        System.out.println(evenOrOdd(6));
+        int[] array = {2,5,7,3,67,23,78,256};
+        System.out.println(sumArray(array));
     }
 }
