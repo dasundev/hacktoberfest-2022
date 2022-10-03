83f5edde94488a4ff3f4cf69cb59d48e2a3f1d92
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-10-03 09:44:57 +0530

    contribution for issue 1

---
+++ java/twinkly.java
@@ -0,0 +1,13 @@
+public class twinkly {
+
+    // Return the Sum of Two Numbers
+    public static int findSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        // Print the Sum of Two Numbers
+        System.out.println(findSum(15, 30));   
+    }
+}
