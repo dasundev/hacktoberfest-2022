04fecf1764bc83ac4937ae93067a55397a20fa16
Chamithu Gunawardana <95626480+ch4mi2@users.noreply.github.com>
2022-10-03 04:44:06 +0000

    Volume of a cone - Issue 1

---
+++ java/ch4mi2.java
@@ -0,0 +1,29 @@
+//chamithu-gunawardana[ch4mi2]-hacktobefest2022
+//Issue 1 - Volume of a cone
+import java.util.*;
+public class ch4mi2{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter height:");
+    double height = sc.nextDouble();
+    System.out.print("Enter radius:");
+    double radius = sc.nextDouble();
+  
+    cone c = new cone(height, radius);
+    c.volume();
+    
+  }
+}
+
+ class cone{
+  private double radius;
+  private double height;
+  
+  cone(double height, double radius){
+    this.height = height;
+    this.radius = radius;
+  }
+  public void volume(){
+    System.out.println("Volume of cone = " + ((22/7.0) * (radius * radius) *     (height/3)));
+  }
+}
