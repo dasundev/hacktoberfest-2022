c2d908b37f158723cbe5a90d71fd041a465e39c2
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-10-03 10:23:10 +0530

    contribution for issue #22 - How Many Vowels?

--- java/twinkly.java
+++ java/twinkly.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class twinkly {
 
     // Return the Sum of Two Numbers
@@ -5,9 +7,27 @@ public class twinkly {
         return a + b;
     }
 
+    // How Many Vowels?
+    public static int getVowelCount(String word) {
+        ArrayList<Character> vowels = new ArrayList<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+    
+        int vowelCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (vowels.contains(word.charAt(i))) {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+    }
+
     public static void main(String[] args) {
 
         // Print the Sum of Two Numbers
         System.out.println(findSum(15, 30));   
+
+
+        //Print the Number of Vowels in a Word
+        System.out.println(getVowelCount("twinkly"));
     }
 }
