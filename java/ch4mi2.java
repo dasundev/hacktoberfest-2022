9a26f6671fc9c2183c7b4f346b27c7fc3057b670
Chamithu Gunawardana <95626480+ch4mi2@users.noreply.github.com>
2022-10-03 05:18:44 +0000

    ISSUE#2 - SumofArray

--- java/ch4mi2.java
+++ java/ch4mi2.java
@@ -4,7 +4,7 @@ import java.util.*;
 //main method
 public class ch4mi2{
   public static void main(String args[]){
-/*---------------ISSUE #1 Volume of cone------------------------------*/ 
+/*---------------ISSUE #1 Volume of cone--------------------------------*/
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter height:");
     double height = sc.nextDouble();
@@ -12,6 +12,25 @@ public class ch4mi2{
     double radius = sc.nextDouble();
     cone c = new cone(height, radius);
     c.volume();
+
+
+/*---------------ISSUE #2 Sum of array elements-----------------------*/      System.out.println("Enter the required size of the array : ");
+      Scanner s = new Scanner(System.in);
+      int size = s.nextInt();
+      int myArray[] = new int [size];
+      int sum = 0;
+      System.out.println("Enter the elements of the array one by one ");
+
+      for(int i=0; i<size; i++){
+         myArray[i] = s.nextInt();
+         sum = sum + myArray[i];
+      }
+      System.out.println("Elements of the array are: "+Arrays.toString(myArray));
+      System.out.println("Sum of the elements of the array :" + sum);
+    
+  }
+}
+
 
     
 //class cone for issue #1
