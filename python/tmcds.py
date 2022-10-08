ebd64b87000e68b71e13ed1ac7eef2773e2d7346
Chamath Denuwana <87439740+tmcds@users.noreply.github.com>
2022-10-08 15:10:14 +0530

    #10 - Is the Number Even or Odd?

--- python/tmcds.py
+++ python/tmcds.py
@@ -4,14 +4,4 @@ if (num%2)==0:
     print("The number you entered is an Even number")
 else: print("The number you entered is an Odd number")
 
-13 - Sum Of Odds
 
-import numbers
-
-
-number = int(input("Enter a number: "))
-sum =0
-for number in range(1,number+1):
-    if (number%2!=0):
-        sum += number
-print("The sum is", sum)    
