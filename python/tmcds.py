db317a57d843ec49f260af862e060a0e22e4663b
tmcds <cds.chamath@gmail.com>
2022-10-08 14:58:36 +0530

    'Sum of odd number' task is done

--- python/tmcds.py
+++ python/tmcds.py
@@ -1,5 +1,17 @@
-#Is the Number Even or Odd? #10
-num = int(input("Enter a number: "))
-if (num%2)==0:
-    print("The number you entered is an Even number")
-else: print("The number you entered is an Odd number")
+#10 - Is the Number Even or Odd? #10
+# num = int(input("Enter a number: "))
+# if (num%2)==0:
+#     print("The number you entered is an Even number")
+# else: print("The number you entered is an Odd number")
+
+# 13 - Sum Of Odds
+
+import numbers
+
+
+number = int(input("Enter a number: "))
+sum =0
+for number in range(1,number+1):
+    if (number%2!=0):
+        sum += number
+print("The sum is", sum)    
