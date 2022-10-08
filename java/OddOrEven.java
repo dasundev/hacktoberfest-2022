1e3f065347811278deb2f0c9d83261074927fdc6
Piumir3 <piumi.18@itfac.mrt.ac.lk>
2022-10-09 01:57:41 +0530

    Odd or even

---
+++ java/OddOrEven.java
@@ -0,0 +1,26 @@
+ 
+import java.io.*;
+import java.util.Scanner;
+
+ 
+class OddOrEven {
+
+	 
+	public static void main(String[] args)
+	{
+		 
+		int num = 10;
+
+		 
+		if (num % 2 == 0) {
+
+			 
+			System.out.println("Number is Even");
+		}
+
+		else {
+ 
+			System.out.println("Number is Odd");
+		}
+	}
+}
