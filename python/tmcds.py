d55dd0e32402a94cf95ad6592cfe3b4dde5c85af
tmcds <cds.chamath@gmail.com>
2022-10-09 07:49:24 +0530

    #14 'Sum of Numbers in an Array' task is done

--- python/tmcds.py
+++ python/tmcds.py
@@ -1,5 +1,8 @@
-#10 - Is the Number Even or Odd? 
-num = int(input("Enter a number: "))
-if (num%2)==0:
-    print("The number you entered is an Even number")
-else: print("The number you entered is an Odd number")
+# 13 - Sum Of Odds
+number = int(input("Enter a number: "))
+sum =0
+for number in range(1,number+1):
+    if (number%2!=0):
+        sum += number
+print("The sum is", sum)
+
