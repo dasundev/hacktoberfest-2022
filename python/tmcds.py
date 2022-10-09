66b0e1da5f56a6614841517f0c7070ad003369b6
tmcds <cds.chamath@gmail.com>
2022-10-09 08:23:27 +0530

    #14 'Find the Largest Number in an Array ' task is done

--- python/tmcds.py
+++ python/tmcds.py
@@ -1,13 +1,17 @@
-#14 Sum of Number in an Array
+#Find the Largest Number in an Array 
 from array import *
 arraySize = int(input("Enter the size of Array : "))
 arr=array("i",[])
-sum=0
 for x in range(arraySize):
     print(f"Input {x +1} value of Array  ")
     y=int(input( ":" ))
     arr.append(y)
-    sum+=arr[x]
-print("Sum of the array is : ", sum)
+
+ln = arr[0]
+for y in range(arraySize):
+       if ln <=arr[y]:
+            ln =arr[y]
+    
+print("Largest number of the array is : ", ln)
 
      
