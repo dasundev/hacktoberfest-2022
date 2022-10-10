de87e006643a6368ceb37ab71416ddd3a102812f
Piyumal98 <59592735+Piyumal98@users.noreply.github.com>
2022-10-10 22:59:31 +0530

    Find the Highest Integer in the Array Using Recursion

--- python/Piyumal98.py
+++ python/Piyumal98.py
@@ -5,3 +5,21 @@ pie = 3.14285714286
 
 volume = pie*(radius*radius)*height/3
 print("volume of the cone = "+str(volume))
+
+
+# Find the Highest Integer in the Array Using Recursion
+def findMaxRec(A, n):
+    if (n == 1):
+        return A[0]
+    return max(A[n - 1], findMaxRec(A, n - 1))
+
+if __name__ == "__main__":
+     
+    A = []
+    num = int(input("Enter number of elements : "))
+    for i in range(0, num):
+        ele = int(input())
+        A.append(ele)
+
+    n = len(A)
+    print("Highest integer is : "+str(findMaxRec(A, n)))
