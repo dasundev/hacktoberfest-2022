cfb663a0a5c003cb043881d21f014cf3e27d548d
Piyumal98 <59592735+Piyumal98@users.noreply.github.com>
2022-10-10 22:38:42 +0530

    add volume of a cone

---
+++ python/Piyumal98.py
@@ -0,0 +1,7 @@
+# volume of a cone
+height = int(input("Enter the height of the cone : "))
+radius = int(input("Enter the radius of the cone : "))
+pie = 3.14285714286 
+
+volume = pie*(radius*radius)*height/3
+print("volume of the cone = "+str(volume))
