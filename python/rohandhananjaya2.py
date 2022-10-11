d2756d60c063c568d5d0cc9cbd9887a316d9774a
Rohan Dhananjaya <rohandhananjaya@gmail.com>
2022-10-11 22:43:44 +0530

    Volume of a cone

---
+++ python/rohandhananjaya2.py
@@ -0,0 +1,10 @@
+#Volume of a cone
+
+PI = 3.14
+
+rad=float(input("Enter the radius of the cone : "))
+hgt=float(input("Enter the height of the cone : "))
+vol=(PI*rad*rad*hgt)/3
+print("Volume of the cone is : ", end = '')
+print(vol)
+    
