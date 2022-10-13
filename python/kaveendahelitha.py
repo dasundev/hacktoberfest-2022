42026df12954855fe8d1ace0ace2e2d9fb4a0bd9
kaveendahelitha <helitha40@gmail.com>
2022-10-13 17:43:25 +0530

    Update kaveendahelitha.py
    volume of cone

--- python/kaveendahelitha.py
+++ python/kaveendahelitha.py
@@ -7,3 +7,13 @@ w = float(input("Enter Width of Rectangle :"))
 a = w*l
 
 print(a)
+
+
+#Volume of a Cone #23
+
+r = float(input("enter radius :"))
+h = float(input("enter height :"))
+fi = float(3.14)
+v = (fi * (r**2) * (h/3.0))
+
+print(v)
