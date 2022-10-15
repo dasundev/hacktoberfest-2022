3a6bb93abb2840911fa83713879b24cdf29d7155
Charithna-Laknara <115604865+Charithna-Laknara@users.noreply.github.com>
2022-10-15 23:00:35 +0530

    Sum of Odd Numbers c++

--- c++/Charithna-Laknara.cpp
+++ c++/Charithna-Laknara.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,max,sum = 0;
+	cout<<"\n Enter the Odd numbers maximum limit: ";
+	cin>>max;
+	
+	cout<<"\nOdd numbers between 0 and "<< max <<" = ";
+	for(n=1;n<=max;n++){
+		if(n%2 != 0){
+			cout<<n<<"";
+			cout<<",";
+			sum = sum + n;
+			}
+	}
+	
+	cout<<"\nThe sum of Odd numbers upto "<< max <<" = "<< sum <<endl;
+	return 0;
 
+}
