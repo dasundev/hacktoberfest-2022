ca8d16d9cfd7b9e8a72a85a4780486ffc2facb3e
Kanchana Chathuranga <73539093+kanchanachathuranga@users.noreply.github.com>
2022-10-16 18:52:58 +0530

    add Sum of the Odd Numbers
    python file

---
+++ python/knchuka1.py
@@ -0,0 +1,9 @@
+#Sum of the Odd Numbers
+value = int(input("please enter the value : "))
+Oddtotal = 0
+for num in range(1, value + 1):
+    if (num % 2!=0):
+        print("{0}".format(num))
+        Oddtotal = Oddtotal + num
+
+print("Sum of odd numbers from 1 to {0} = {1}".format(num, Oddtotal))
