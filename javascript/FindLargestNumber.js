b6bf414b4008e7a7145ab6bf5fcf81347e0c5186
Bhashitha Ranasinghe <73308863+hbsbashitha@users.noreply.github.com>
2022-10-18 22:23:33 +0530

    fix error

--- javascript/FindLargestNumber.js
+++ javascript/FindLargestNumber.js
@@ -1,7 +1,7 @@
 let numberArray = [1, 2, 34, 54, 65, 32, 56, 67, 23, 76, 20]
 
 Array.prototype.findLargest = function () {
-    let maxValue = numberArray[0]
+    let maxValue =  numberArray[0]
     for (let i = 1; i < numberArray.length; i++) {
         if (maxValue < numberArray[i]) {
             maxValue = numberArray[i]
