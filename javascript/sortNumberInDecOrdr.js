c21c9d407ba7fc76b89832932f687d6a240f6079
Bhashitha Ranasinghe <73308863+hbsbashitha@users.noreply.github.com>
2022-10-18 22:34:29 +0530

    correct error

--- javascript/sortNumberInDecOrdr.js
+++ javascript/sortNumberInDecOrdr.js
@@ -1,17 +1,17 @@
 let numberArray = [1, 2, 34, 54, 65, 32, 56, 67, 23, 76]
 
 Array.prototype.descOrder = function () {
-    for (let i = 0; i < this.length; i++) {
-        for (let j = i + 1; j < this.length; j++) {
-            if (this[i] < this[j]) {
-                let temp = this[i];
-                this[i] = this[j];
-                this[j] = temp;
+    for (let i = 0; i < numberArray.length; i++) {
+        for (let j = i + 1; j < numberArray.length; j++) {
+            if (numberArray[i] < numberArray[j]) {
+                let temp = numberArray[i];
+                numberArray[i] = numberArray[j];
+                numberArray[j] = temp;
             }
         }
     }
 
-    return this
+    return numberArray
 
 }
 
