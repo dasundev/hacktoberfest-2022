05f92ebf3bbe55ba3eee55bca7d3aa33781c010e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-18 23:03:00 +0530

    Is the Number Even or Odd

---
+++ javascript/NimeshPiyumantha.js
@@ -0,0 +1,14 @@
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+function oddOrEven(x) {
+    return ( x & 1 ) ? "odd" : "even";
+}
+
+function checkNumber(argNumber) {
+    document.getElementById("result").innerHTML = "Number " + argNumber + " is " + oddOrEven(argNumber);
+}
+
+checkNumber(17);
