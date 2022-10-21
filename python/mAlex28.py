872957f5c3c44a8c2592ed77bc01bac3b9ebbe79
Alex <alexthegeek2001@gmail.com>
2022-10-21 13:27:47 +0530

    sort numbers in descending order

---
+++ python/mAlex28.py
@@ -0,0 +1,8 @@
+# Sort Numbers in Descending Order
+
+# Assumption: None negative numbers will not be entered
+
+def sortForDescendingOrder(list):
+    return sorted(list, reverse=True)
+    
+sortForDescendingOrder([10, 300, 34, 64.40, 500])
