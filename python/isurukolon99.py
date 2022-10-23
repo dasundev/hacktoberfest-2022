9099dc238741678d76877a82ebaa0ebb47ac8656
Gaya39877 <gayathrirasangikahw@gmail.com>
2022-10-23 23:19:18 +0530

    new comment

--- python/isurukolon99.py
+++ python/isurukolon99.py
@@ -7,4 +7,23 @@ for num in range(1, max + 1):
         Oddtotal = Oddtotal + num
         
 print("Sum of odd numbers from 1 to {0} = {1}".format(num, Oddtotal))
+
+
+"""num = int (input (“Enter any number to test whether it is odd or even: “)
+if (num % 2) == 0:
+
+              print ("The number is even")
+
+else:
+
+              print ("The provided number is odd")
+
+Output:
+
+Enter any number to test whether it is odd or even:
+
+887
+
+887 is odd. """
+
         
