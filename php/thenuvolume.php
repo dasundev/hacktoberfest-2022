5804e500b923d4e6f8f3f1c56eb37eb817106b7f
Thenu <64696941+ThenuSand@users.noreply.github.com>
2022-10-24 20:23:23 +0530

    Update and rename thenuvolume.php.txt to thenuvolume.php

--- php/thenuvolume.php
+++ php/thenuvolume.php
@@ -1,6 +1,6 @@
 <?php   
-   $height=38;  
-    $radius=35;  
+   $height=height of the cone;  
+    $radius=radius of the base;  
     $pie=3.14285714286;  
     $volume=$pie*($radius*$radius)*$height/3;  
     echo "Volume of the cone=";  
