ec2a2e0090b6d538a43dbff9a7fe71273785fdd4
IT20617332 <siridana@gmail.com>
2022-10-25 13:48:50 +0530

    finding the largest number in the array implementation added

--- python/IT20617332.py
+++ python/IT20617332.py
@@ -10,7 +10,7 @@ else:
 
 #finding sum of numbers in an array #2
     
-arr = [2, 9, 3, 4, 5];     
+arr = [2, 9, 3, 4, 6];     
 sum = 0;    
      
 #Loop through the array to calculate sum of elements    
@@ -18,3 +18,20 @@ for i in range(0, len(arr)):
    sum = sum + arr[i];    
      
 print("Sum of all the elements of an array: " + str(sum)); 
+
+
+#finding largest in an array #3
+
+#Initialize array     
+arr = [25, 11, 7, 113, 56];     
+         
+#Initialize max with first element of array.    
+max = arr[0];    
+         
+#Loop through the array    
+for i in range(0, len(arr)):    
+    #Compare elements of array with max    
+    if(arr[i] > max):    
+        max = arr[i];    
+               
+print("Largest element present in given array: " + str(max));   
