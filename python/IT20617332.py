8d3effb596653957e9f06de67fc473aa8868feb5
IT20617332 <siridana@gmail.com>
2022-10-25 13:39:54 +0530

    finding sum of numbers in an array added

--- python/IT20617332.py
+++ python/IT20617332.py
@@ -7,3 +7,14 @@ if mod > 0:
 else:
     print("This is an even number.");
 
+
+#finding sum of numbers in an array #2
+    
+arr = [2, 9, 3, 4, 5];     
+sum = 0;    
+     
+#Loop through the array to calculate sum of elements    
+for i in range(0, len(arr)):    
+   sum = sum + arr[i];    
+     
+print("Sum of all the elements of an array: " + str(sum)); 
