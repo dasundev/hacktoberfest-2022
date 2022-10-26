70d46980775492996664d6479b2d6ee9c1c8f2aa
Shanaka Asiri <you@example.com>
2022-10-26 12:22:09 +0530

    sum of the odd numbers

--- javascript/shanasiri.js
+++ javascript/shanasiri.js
@@ -6,3 +6,14 @@ for (let i = 0; i < array.length; i++) {
     sum += array[i];
 }
 console.log(sum);
+
+//Sum of the odd numbers
+limit = 10;
+sum = 0;
+
+for(let i = 1; i <= 10; i++){
+    if(i % 2 == 1){
+        sum = sum + i;
+    }
+}
+console.log(`The sum of odd numbers from 0 - ${limit} is: \n ${sum}`);
