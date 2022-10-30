2633001d1dc1885e50048c2e5da95fba507055b2
TharushiGunarathna <98076644+TharushiGunarathna@users.noreply.github.com>
2022-10-30 11:56:36 +0530

    Create tharushigunarathna.py

---
+++ python/tharushigunarathna.py
@@ -0,0 +1,10 @@
+#Is the Number Even or Odd?
+num = int (input (“Enter a number: “)
+
+if (num % 2) == 0:
+
+              print (“The entered number is even”)
+
+else:
+
+              print (“The entered number is odd”)
